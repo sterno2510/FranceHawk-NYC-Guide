95d655cefa6fd15990b79408879b429c57e37f50
yahighness <franceshameeka@gmail.com>
2021-11-11 22:12:16 -0500

    added venue to views and venue.html

--- nyc-guide-main/nyc/views.py
+++ nyc-guide-main/nyc/views.py
@@ -10,8 +10,11 @@ def borough(request, borough):
         return render(request=request, template_name='borough.html', context={ 'borough': borough, 'activities': boroughs[borough].keys() })
 
 # Create your views here.
-def activity():
-    pass
+def activity(request, borough, activity):
+    if request.method == 'GET':
+        return render(request=request, template_name='activity.html', context={ 'borough': borough, 'activity': activity, 'venue': boroughs[borough][activity].keys() })
 
-def venue():
-    pass
+def venue(request, borough, activity, venue):
+    if request.method == 'GET':
+        return render(request=request, template_name='venue.html', context={'borough': borough,'activity': activity, 'venue': venue, 'description': boroughs[borough][activity][venue].get('description')})
+
